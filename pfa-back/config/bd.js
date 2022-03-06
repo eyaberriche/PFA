5af86c0d383a10f2b8a17caeed701022cc2eab41
Eya Berriche <eyaberriche2@gmail.com>
2022-03-06 19:20:24 +0100

    models and ddbb

--- pfa-back/config/bd.js
+++ pfa-back/config/bd.js
@@ -1,14 +1,1 @@
-var mongoose = require("mongoose");
-//environment variables
-require("dotenv").config();
-//database connection
 
-function connectDb() {
-  const uri = process.env.ATLAS_URI;
-  mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-  const connection = mongoose.connection;
-  connection.once("open", () => {
-    console.log("Connected Database Successfully");
-  });
-}
-module.exports = connectDb;
