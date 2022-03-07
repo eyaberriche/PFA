a86c901d5b05f973bcb7c476a75cf74b79a4992d
Eya Berriche <eyaberriche2@gmail.com>
2022-03-07 15:05:47 +0100

    login and register version finale

--- pfa-back/controllers/apiController.js
+++ pfa-back/controllers/apiController.js
@@ -3,33 +3,36 @@ const jwt = require("jsonwebtoken");
 var User = require("../models/user");
 var bcrypt = require("bcrypt");
 
-exports.registerUser = (req, res) => {
-  var user = new User({
-    email: req.body.email,
-    password: User.hashPassword(req.body.p1),
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-  });
-  User.find({ email: req.body.email }, (err, users) => {
-    if (err) {
-      console.log("err in finding email ");
-      res.json({ msg: "some error!" });
-    }
-    if (users.length != 0) {
-      console.log("already user with this email");
-      res.json({ msg: "already user exist with this email!" });
-    } else {
-      user.save((error, registeredUser) => {
-        if (error) {
-          console.log(error);
-          res.json({ msg: "some error!" });
-        } else {
-          let payload = { subject: registeredUser._id };
-          let token = jwt.sign(payload, "secretkey");
-          res.status(200).json({ token: token });
-        }
-      });
-    }
+exports.registerUser = async (req, res) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    var userr = new User({
+      lastname: req.body.lastname,
+      firstname: req.body.firstname,
+      email: req.body.email,
+      password: hash,
+    });
+
+    User.find({ email: req.body.email }, (err, users) => {
+      if (err) {
+        console.log("err in finding email ");
+        res.json({ msg: "some error!" });
+      }
+      if (users.length != 0) {
+        console.log("already user with this email");
+        res.json({ msg: "already user exist with this email!" });
+      } else {
+        userr.save((error, registeredUser) => {
+          if (error) {
+            console.log(error);
+            res.json({ msg: "some error!" });
+          } else {
+            let payload = { subject: registeredUser._id };
+            let token = jwt.sign(payload, "secretkey");
+            res.status(200).json({ token: token });
+          }
+        });
+      }
+    });
   });
 };
 
@@ -43,7 +46,8 @@ exports.logIn = (req, res) => {
         res.json({ msg: "Invalid Email!!" });
       } else {
         bcrypt
-          .compare(req.body.p1, user.password)
+          .compare(req.body.password, user.password)
+
           .then((match) => {
             if (match) {
               console.log("login sucesssss");
@@ -61,8 +65,8 @@ exports.logIn = (req, res) => {
             }
           })
           .catch((err) => {
-            console.log("somthing wrong");
-            res.json({ msg: "Somthing went wrong" });
+            console.log(err);
+            res.json({ msg: "mochkla" });
           });
       }
     }
--- pfa-back/routes/api.js
+++ pfa-back/routes/api.js
@@ -1,9 +1,13 @@
 // routes mtaa lhajet l publiques
 var express = require("express");
 var router = express.Router();
+var bodyParser = require("body-parser");
+
+// create application/json parser
+var jsonParser = bodyParser.json();
 const apiController = require("../controllers/apiController");
 
-router.post("/register", apiController.registerUser);
-router.post("/login", apiController.logIn);
+router.post("/register", jsonParser, apiController.registerUser);
+router.post("/login", jsonParser, apiController.logIn);
 
 module.exports = router;
